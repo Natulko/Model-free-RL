1b4ef4f5862cee62d6bdf73e834f208a8bbe7484
Max Lytovka <litmax515@gmail.com>
2024-03-25 21:54:41 +0100

    1c

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -25,37 +25,43 @@ class QLearningAgent(object):
 
 class SARSAAgent(object):
 
-    def __init__(self, n_actions, n_states, epsilon):
-        self.n_actions = n_actions
-        self.n_states = n_states
+    def __init__(self, env, epsilon, alpha):
+        self.n_actions = env.action_size()
+        self.n_states = env.state_size()
         self.epsilon = epsilon
-        # TO DO: Add own code
-        pass
+        self.alpha = alpha
+        self.gamma = 1  # gamma is 1 based on description of the task
+        self.Q = np.zeros((env.state_size(), env.action_size()))
 
     def select_action(self, state):
-        # TO DO: Add own code
-        a = random.choice(range(self.n_actions))  # Replace this with correct action selection
+        a = np.argmax(self.Q[state])  # greedy action
+        if random.random() < self.epsilon:
+            a = random.choice(range(self.n_actions))
         return a
 
-    def update(self, state, action, reward):
-        # TO DO: Add own code
-        pass
+    def update(self, state, action, reward, state_prime):
+        self.Q[state, action] = self.Q[state, action] + self.alpha * \
+            (reward + self.gamma * self.Q[state_prime, self.select_action(state_prime)] - self.Q[state, action])
 
 
 class ExpectedSARSAAgent(object):
 
-    def __init__(self, n_actions, n_states, epsilon):
-        self.n_actions = n_actions
-        self.n_states = n_states
+    def __init__(self, env, epsilon, alpha):
+        self.n_actions = env.action_size()
+        self.n_states = env.state_size()
         self.epsilon = epsilon
-        # TO DO: Add own code
-        pass
+        self.alpha = alpha
+        self.gamma = 1  # gamma is 1 based on description of the task
+        self.Q = np.zeros((env.state_size(), env.action_size()))
 
     def select_action(self, state):
-        # TO DO: Add own code
-        a = random.choice(range(self.n_actions))  # Replace this with correct action selection
+        a = np.argmax(self.Q[state])  # greedy action
+        if random.random() < self.epsilon:
+            a = random.choice(range(self.n_actions))
         return a
 
-    def update(self, state, action, reward):
-        # TO DO: Add own code
-        pass
+    def update(self, state, action, reward, state_prime):
+        Q_expected = np.sum(self.Q[state_prime] * (self.epsilon / self.n_actions)) + \
+            np.max(self.Q[state_prime]) * (1 - self.epsilon)
+        self.Q[state, action] = self.Q[state, action] + self.alpha * \
+            (reward + self.gamma * Q_expected - self.Q[state, action])
--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -2,7 +2,7 @@
 from typing import Union
 import numpy as np
 from ShortCutAgents import QLearningAgent, SARSAAgent, ExpectedSARSAAgent
-from ShortCutEnvironment import ShortcutEnvironment
+from ShortCutEnvironment import ShortcutEnvironment, WindyShortcutEnvironment
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 
@@ -88,7 +88,7 @@ def print_greedy_actions(Q):
 
 # Experiment functions
 def run_repetition(
-        env: ShortcutEnvironment,
+        env: Union[ShortcutEnvironment, WindyShortcutEnvironment],
         agent: Union[QLearningAgent, SARSAAgent, ExpectedSARSAAgent],
         n_episodes: int,
         n_steps: int
@@ -120,14 +120,13 @@ def run_repetitions(
         **kwargs
 ) -> np.array:
     curve = np.zeros(n_episodes)
-    agent = None
     for _ in range(n_repetitions):
         if agent_type == "q-learning":
             agent = QLearningAgent(environment, epsilon=kwargs["epsilon"], alpha=kwargs["alpha"])
         elif agent_type == "SARSA":
-            pass
+             agent = SARSAAgent(environment, epsilon=kwargs["epsilon"], alpha=kwargs["alpha"])
         else:
-            pass
+             agent = ExpectedSARSAAgent(environment, epsilon=kwargs["epsilon"], alpha=kwargs["alpha"])
         repetition_curve = run_repetition(env, agent, n_episodes=n_episodes, n_steps=n_steps)
         env.reset()  # just double-check, it should be also done in the run_repetitions
         curve += repetition_curve
@@ -136,14 +135,28 @@ def run_repetitions(
 
 def experiment(
         env: ShortcutEnvironment,
+        windy_env: WindyShortcutEnvironment,
         n_repetitions: int,
         n_episodes: int,
+        n_episodes_single: int,
         n_steps: int,
         smoothing_window: int,
         epsilon: float,
         alphas: list,
 ):
     # Assignment 1 - Q-learning
+
+    # single experiment
+    alpha = 0.1
+    q_agent = QLearningAgent(env, epsilon=epsilon, alpha=alpha)
+    run_repetition(env, q_agent, n_episodes=n_episodes_single, n_steps=n_steps)
+    print("Q-LEARNING GREEDY POLICY: \n")
+    print_greedy_actions(q_agent.Q)
+    print("\n")
+
+    # different alpha values
+    q_agent_max_val = -np.inf
+    q_agent_best_alpha = alphas[0]
     QLearn_plot = LearningCurvePlot(title="Q-learning learning curves based on alpha hyperparam.")
     for i in range(len(alphas)):
         alpha = alphas[i]
@@ -157,14 +170,138 @@ def experiment(
             alpha=alpha
         )
         QLearn_plot.add_curve(smooth(curve, smoothing_window), f"alpha={alpha}")
-    QLearn_plot.save("Q-Learning plot")
+        if curve[-1] > q_agent_max_val:
+            q_agent_max_val = curve[-1]
+            q_agent_best_alpha = alpha
+    QLearn_plot.save("Q-Learning_plot")
+
+    # Assignment 2 - SARSA
+
+    # single experiment
+    alpha = 0.1
+    sarsa_agent = SARSAAgent(env, epsilon=epsilon, alpha=alpha)
+    run_repetition(env, sarsa_agent, n_episodes=n_episodes_single, n_steps=n_steps)
+    print("SARSA GREEDY POLICY: \n")
+    print_greedy_actions(sarsa_agent.Q)
+    print("\n")
+
+    # different alpha values
+    sarsa_agent_max_val = -np.inf
+    sarsa_agent_best_alpha = alphas[0]
+    SARSA_plot = LearningCurvePlot(title="SARSA learning curves based on alpha hyperparam.")
+    for i in range(len(alphas)):
+        alpha = alphas[i]
+        curve = run_repetitions(
+            env=env,
+            agent_type="SARSA",
+            n_repetitions=n_repetitions,
+            n_episodes=n_episodes,
+            n_steps=n_steps,
+            epsilon=epsilon,
+            alpha=alpha
+        )
+        SARSA_plot.add_curve(smooth(curve, smoothing_window), f"alpha={alpha}")
+        if curve[-1] > sarsa_agent_max_val:
+            sarsa_agent_max_val = curve[-1]
+            sarsa_agent_best_alpha = alpha
+    SARSA_plot.save("SARSA_plot")
+
+    # Assignment 3 - Windy weather!
+    alpha = 0.1
+
+    q_agent_wind = QLearningAgent(windy_env, epsilon=epsilon, alpha=alpha)
+    run_repetition(windy_env, q_agent_wind, n_episodes=n_episodes_single, n_steps=n_steps)
+    sarsa_agent_wind = SARSAAgent(windy_env, epsilon=epsilon, alpha=alpha)
+    run_repetition(windy_env, sarsa_agent_wind, n_episodes=n_episodes_single, n_steps=n_steps)
+
+    print("~~~ WINDY WEATHER ~~~\n")
+
+    print("Q-LEARNING GREEDY POLICY WITH WIND:")
+    print_greedy_actions(q_agent_wind.Q)
+    print("\n")
+    print("SARSA GREEDY POLICY WITH WIND:")
+    print_greedy_actions(sarsa_agent_wind.Q)
+    print("\n")
+
+    # Assignment 4 - Expected SARSA
+
+    # single experiment
+    alpha = 0.1
+    exp_sarsa_agent = ExpectedSARSAAgent(env, epsilon=epsilon, alpha=alpha)
+    run_repetition(env, exp_sarsa_agent, n_episodes=n_episodes_single, n_steps=n_steps)
+    print("EXPECTED SARSA GREEDY POLICY: \n")
+    print_greedy_actions(exp_sarsa_agent.Q)
+    print("\n")
+
+    # different alpha values
+    exp_sarsa_agent_max_val = -np.inf
+    exp_sarsa_agent_best_alpha = alphas[-1]
+    Ex_SARSA_plot = LearningCurvePlot(title="Expected SARSA learning curves based on alpha hyperparam.")
+    for i in range(len(alphas)):
+        alpha = alphas[i]
+        curve = run_repetitions(
+            env=env,
+            agent_type="expected_SARSA",
+            n_repetitions=n_repetitions,
+            n_episodes=n_episodes,
+            n_steps=n_steps,
+            epsilon=epsilon,
+            alpha=alpha
+        )
+        Ex_SARSA_plot.add_curve(smooth(curve, smoothing_window), f"alpha={alpha}")
+        if curve[-1] < exp_sarsa_agent_max_val:
+            exp_sarsa_agent_max_val = curve[-1]
+            exp_sarsa_agent_best_alpha = alpha
+    Ex_SARSA_plot.save("Exp_SARSA_plot")
+
+    # Comparison of the methods
+    comparison_plot = LearningCurvePlot(title="Comparison plot of the tuned models")
+    q_curve = run_repetitions(
+        env=env,
+        agent_type="q-learning",
+        n_repetitions=n_repetitions,
+        n_episodes=n_episodes,
+        n_steps=n_steps,
+        epsilon=epsilon,
+        alpha=q_agent_best_alpha
+    )
+    comparison_plot.add_curve(
+        smooth(q_curve, smoothing_window), f"Q_learning (alpha={q_agent_best_alpha})"
+    )
+    sarsa_curve = run_repetitions(
+        env=env,
+        agent_type="SARSA",
+        n_repetitions=n_repetitions,
+        n_episodes=n_episodes,
+        n_steps=n_steps,
+        epsilon=epsilon,
+        alpha=sarsa_agent_best_alpha
+    )
+    comparison_plot.add_curve(
+        smooth(sarsa_curve, smoothing_window), f"SARSA (alpha={sarsa_agent_best_alpha})"
+    )
+    exp_sarsa_curve = run_repetitions(
+        env=env,
+        agent_type="expected_SARSA",
+        n_repetitions=n_repetitions,
+        n_episodes=n_episodes,
+        n_steps=n_steps,
+        epsilon=epsilon,
+        alpha=exp_sarsa_agent_best_alpha
+    )
+    comparison_plot.add_curve(
+        smooth(exp_sarsa_curve, smoothing_window), f"Exp. SARSA (alpha={exp_sarsa_agent_best_alpha})"
+    )
+    comparison_plot.save("Comparison")
 
 
 if __name__ == "__main__":
     # Setting up the parameters of the experiment
     environment = ShortcutEnvironment()
-    n_repetitions = 50
-    n_episodes = 100
+    windy_environment = WindyShortcutEnvironment()
+    n_repetitions = 100
+    n_episodes = 1_000
+    n_episodes_single = 10_000
     n_steps = 100
     alphas = [0.01, 0.1, 0.5, 0.9]
     epsilon = 0.1
@@ -172,8 +309,10 @@ if __name__ == "__main__":
     # The experiment itself
     experiment(
         env=environment,
+        windy_env=windy_environment,
         n_repetitions=n_repetitions,
         n_episodes=n_episodes,
+        n_episodes_single=n_episodes_single,
         n_steps=n_steps,
         alphas=alphas,
         epsilon=epsilon,
